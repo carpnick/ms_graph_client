14b27976cf0fb6b6909c948829cf1ebcabe82ebc
Nick Carpenter <carpnick@users.noreply.github.com>
2025-03-12 12:30:12 -0400

    Increase cache size

--- src/ms_graph_client/services_cache/users.py
+++ src/ms_graph_client/services_cache/users.py
@@ -8,7 +8,7 @@ class Users:
     def __init__(self, crud_client: UsersCrud):
         self._crud_client = crud_client
 
-    @cachetools.func.ttl_cache(ttl=600)
+    @cachetools.func.ttl_cache(ttl=600, maxsize=500)
     def get_user(self, upn: str, select_properties: Optional[frozenset[str]] = None) -> Any:
         my_list = None
         if select_properties is not None:
@@ -16,7 +16,7 @@ class Users:
         res = self._crud_client.get_user(upn=upn, select_properties=my_list)
         return res
 
-    @cachetools.func.ttl_cache(ttl=600)
+    @cachetools.func.ttl_cache(ttl=600, maxsize=500)
     def get_manager(self, upn: str) -> Any:
         res = self._crud_client.get_manager(upn=upn)
         return res
